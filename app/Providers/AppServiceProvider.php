ba0a6bddec572f11a7efaf5ea07dab58b7aff893
Timothy Mulei <muleitimothy@gmail.com>
2025-06-21 18:46:39 +0300

    corrected https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,4 +21,15 @@ class AppServiceProvider extends ServiceProvider
     {
         //
     }
+
+
+    public function boot()
+    {
+        if (env('APP_ENV') == 'production') {
+            $this->app['request']->server->set('HTTPS', true);
+        }
+    }
+
+
+
 }
